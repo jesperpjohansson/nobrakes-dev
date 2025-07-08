e9d04b107d127164c8a40fa1b9c6bee16482ece4
jesperpjohansson <jesperpjohansson@gmail.com>
2025-07-08 22:40:46 +0200

    Remove opargs from test script

--- scripts/test.py
+++ scripts/test.py
@@ -1,18 +1,6 @@
-import argparse
 import subprocess
-import sys
 
 from scripts._utils import check_dependencies_installed
-
-PARSER = argparse.ArgumentParser()
-PARSER.add_argument(
-    "-o",
-    "--opargs",
-    nargs="+",
-    help='optional pytest args, without the "--" prefix',
-    default=(),
-)
-ARGS = PARSER.parse_args()
 
 dependencies = {
     "nobrakes": "nobrakes",
@@ -23,15 +11,8 @@ dependencies = {
     "pyyaml": "yaml",
 }
 
-if any(str.startswith(arg, "cov") for arg in ARGS.opargs):
-    dependencies |= {"pytest-cov": "pytest_cov"}
-
 check_dependencies_installed("test", dependencies)
 
 input_ = ["pytest", "tests"]
 
-if ARGS.opargs:
-    input_.extend("--" + arg for arg in ARGS.opargs)
-
-code = subprocess.run(input_, check=False).returncode
-sys.exit(code)
+code = subprocess.run(input_, check=True)
