2154e4d006c55395ec70440b80f6e21c60986458
jesperpjohansson <jesperpjohansson@gmail.com>
2025-07-28 17:14:05 +0200

    Add docstring

--- nobrakes/__init__.py
+++ nobrakes/__init__.py
@@ -1,4 +1,28 @@
-"""....."""
+# ruff: noqa: D400
+"""
+`nobrakes`
+
+A high-level user API for asynchronous fetching, parsing, and transforming
+Swedish speedway data sourced from SVEMO.
+
+Classes
+-------
+SVEMOScraper
+    An asynchronous scraper for speedway data sourced from SVEMO.
+
+Modules & Subpackages
+---------------------
+client
+    Tools for adapting third-party asynchronous HTTP libraries.
+exceptions
+    Exceptions used throughout the `nobrakes` library.
+pgelements
+    Typed dictionaries representing parsed HTML elements from SVEMO pages.
+pgmodel
+    Data transformation models for `nobrakes.SVEMOScraper` output.
+typing
+    Typing used throughout the `nobrakes` API.
+"""
 
 from nobrakes import client, pgelements, pgmodel, typing
 from nobrakes._scraper import SVEMOScraper
